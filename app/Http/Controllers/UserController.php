4891b0a9166b65124fbbe4847fd06beecdb62b24
zaee07 <ihzasofyansah@gmail.com>
2024-06-04 23:10:33 +0700

    make userController + route login

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login() {
+        
+    }
+
+    public function doLogin() {
+        
+    }
+
+    public function doLogout() {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,9 @@ Route::get('/', function () {
 });
 
 Route::view('template', 'template');
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/login', 'login');
+    Route::post('/login', 'doLogin');
+    Route::post('/logout', 'doLogout');
+});
