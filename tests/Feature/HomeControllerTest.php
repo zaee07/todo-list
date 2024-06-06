45f020dee165ab8726ef4ec5a363c2e708e567af
zaee07 <ihzasofyansah@gmail.com>
2024-06-07 00:16:39 +0700

    make home controller+page

---
+++ tests/Feature/HomeControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeControllerTest extends TestCase
+{
+    public function testGuest() {
+        $this->get('/')->assertRedirect('/login');
+    }
+
+    public function testMember() {
+        $this->withSession(['user' => 'ihza'])
+        ->get('/')->assertRedirect('/todolist');
+    }
+}
