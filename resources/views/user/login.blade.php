977b2472337dd7b380834d998e15120714f89ae5
Muhammad Ihza Sofyansyah <111619723+zaee07@users.noreply.github.com>
2024-06-08 23:42:49 +0700

    Edit Author

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -21,8 +21,7 @@
     <div class="row align-items-center g-lg-5 py-5">
         <div class="col-lg-7 text-center text-lg-start">
             <h1 class="display-4 fw-bold lh-1 mb-3">Login</h1>
-            <p class="col-lg-10 fs-4">by <a target="_blank" href="https://www.programmerzamannow.com/">Programmer Zaman
-                    Now</a></p>
+            <p class="col-lg-10 fs-4">by <a target="_blank" href="https://zaee07.my.id/">Zaee_07</a></p>
         </div>
         <div class="col-md-10 mx-auto col-lg-5">
             <form class="p-4 p-md-5 border rounded-3 bg-light" method="post" action="/login">
